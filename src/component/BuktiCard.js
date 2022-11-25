0bc64e59251d7f65e83a7a398ddedcac2a85a7f0
FahrezaIsnanto <mfahrezaisnanto@students.undip.ac.id>
2022-11-25 11:07:25 +0700

    add alt to qr code

--- src/component/BuktiCard.js
+++ src/component/BuktiCard.js
@@ -5,7 +5,7 @@ export default function BuktiCard({data}){
         <table border="0" width="100%">
             <tr>
                 <td>{data.id}</td>
-                <td rowSpan={5} className="qrCode"><img src={`https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs=150x150&chl=${data.id}`}></img></td>
+                <td rowSpan={5} alt="qrCode" className="qrCode"><img src={`https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs=150x150&chl=${data.id}`}></img></td>
             </tr>
             <tr>
                 <td>{data.nama_pasien}</td>
